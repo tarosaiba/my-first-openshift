e90917059d498d5fb1c54d6ebf022342c43bc839
tarosaiba <tarosan.s1117@gmail.com>
2021-11-29 13:06:49 +0900

    fix

--- init.sh
+++ init.sh
@@ -2,4 +2,12 @@
 oc create -f operator.yaml
 oc create -f projects.yaml
 
+# Tekton
+oc create -f tekton
+oc adm policy add-scc-to-user privileged -nmy-first-openshift-common -z pipeline
 
+# Argo
+kubectl -n openshift-gitops get secret openshift-gitops-cluster -o 'go-template={{index .data "admin.password"}}' | base64 -d 
+oc create -f argo/appprojects
+oc create -f argo/applications
+oc adm policy add-role-to-user admin system:serviceaccount:openshift-gitops:openshift-gitops-argocd-application-controller -n my-first-openshift-dev

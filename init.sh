fb80a277051e8a275437dc4dde58ac04042e237d
tarosaiba <tarosan.s1117@gmail.com>
2021-12-03 15:46:11 +0900

    fix

--- init.sh
+++ init.sh
@@ -4,11 +4,32 @@ oc create -f operator.yaml
 oc create -f default-scc.yaml
 oc create -f tekton
 oc create -f argo
-echo "Argo admin pass: "
-kubectl -n openshift-gitops get secret openshift-gitops-cluster -o 'go-template={{index .data "admin.password"}}' | base64 -d
 
 echo "please wait 60sec.."
 sleep 60
 
-# TODO: run pipelines
 
+#  # TODO: run pipelines
+#  echo "apiVersion: tekton.dev/v1beta1
+#  kind: PipelineRun
+#  metadata:
+#    name: ci-pipeline-services-01
+#    namespace: my-first-openshift-common
+#    labels:
+#      tekton.dev/pipeline: ci-pipeline-services
+#  spec:
+#    pipelineRef:
+#      name: ci-pipeline-services
+#    resources:
+#      - name: appSource
+#        resourceRef:
+#          name: git-service-a
+#      - name: appImage
+#        resourceRef:
+#          name: image-service-a
+#    serviceAccountName: pipeline
+#    timeout: 1h0m0s
+#  " | oc create -f -
+
+echo "Argo admin pass: "
+kubectl -n openshift-gitops get secret openshift-gitops-cluster -o 'go-template={{index .data "admin.password"}}' | base64 -d

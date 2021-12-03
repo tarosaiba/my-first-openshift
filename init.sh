bccd605e22e174601d1ca785781b60623f3d2cca
tarosaiba <tarosan.s1117@gmail.com>
2021-12-03 15:25:36 +0900

    fix

--- init.sh
+++ init.sh
@@ -6,3 +6,9 @@ oc create -f tekton
 oc create -f argo
 echo "Argo admin pass: "
 kubectl -n openshift-gitops get secret openshift-gitops-cluster -o 'go-template={{index .data "admin.password"}}' | base64 -d
+
+echo "please wait 60sec.."
+sleep 60
+
+# TODO: run pipelines
+

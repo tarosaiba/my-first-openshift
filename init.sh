9402f04c25356a706a0089cb5e1939b66423348d
tarosaiba <tarosan.s1117@gmail.com>
2021-12-09 22:35:39 +0900

    fix

--- init.sh
+++ init.sh
@@ -1,35 +1,62 @@
 #!bin/bash
 oc create -f projects.yaml
-oc create -f operator.yaml
 oc create -f default-scc.yaml
+oc create -f operator.yaml
 
-echo "please wait 60sec.."
-sleep 60
+echo "please wait 180sec.."
+sleep 180
 
 oc create -f tekton
 oc create -f argo
 
-#  # TODO: run pipelines
-#  echo "apiVersion: tekton.dev/v1beta1
-#  kind: PipelineRun
-#  metadata:
-#    name: ci-pipeline-services-01
-#    namespace: my-first-openshift-common
-#    labels:
-#      tekton.dev/pipeline: ci-pipeline-services
-#  spec:
-#    pipelineRef:
-#      name: ci-pipeline-services
-#    resources:
-#      - name: appSource
-#        resourceRef:
-#          name: git-service-a
-#      - name: appImage
-#        resourceRef:
-#          name: image-service-a
-#    serviceAccountName: pipeline
-#    timeout: 1h0m0s
-#  " | oc create -f -
+# Run Pipelines
+SUFFIX=`date +"%y%m%d-%H-%M-%S"`
+## service-a
+service="service-a"
+echo "apiVersion: tekton.dev/v1beta1
+kind: PipelineRun
+metadata:
+  name: ci-pipeline-${service}-${SUFFIX}
+  namespace: my-first-openshift-common
+  labels:
+    tekton.dev/pipeline: ci-pipeline-services
+spec:
+  pipelineRef:
+    name: ci-pipeline-services
+  resources:
+    - name: appSource
+      resourceRef:
+        name: git-${service}
+    - name: appImage
+      resourceRef:
+        name: image-${service}
+  serviceAccountName: pipeline
+  timeout: 1h0m0s
+" | oc create -f -
+
+## service-b
+service="service-b"
+echo "apiVersion: tekton.dev/v1beta1
+kind: PipelineRun
+metadata:
+  name: ci-pipeline-${service}-${SUFFIX}
+  namespace: my-first-openshift-common
+  labels:
+    tekton.dev/pipeline: ci-pipeline-services
+spec:
+  pipelineRef:
+    name: ci-pipeline-services
+  resources:
+    - name: appSource
+      resourceRef:
+        name: git-${service}
+    - name: appImage
+      resourceRef:
+        name: image-${service}
+  serviceAccountName: pipeline
+  timeout: 1h0m0s
+" | oc create -f -
+
 
 echo "Argo admin pass: "
 kubectl -n openshift-gitops get secret openshift-gitops-cluster -o 'go-template={{index .data "admin.password"}}' | base64 -d
